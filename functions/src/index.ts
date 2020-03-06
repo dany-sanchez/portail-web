abe2aea62273a209dd300b2f07f32c2c6ececc85
Dany Sanchez <daany_57@hotmail.fr>
2020-03-06 11:24:24 +0100

    :recycle: Ajout user onDelete, fix user onCreate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,25 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 admin.initializeApp();
-exports.triggerUser = functions.auth.user().onCreate((user) => {
+exports.triggerUserCreated = functions.auth.user().onCreate((user) => {
   console.log("user created");
   return admin
     .firestore()
-    .collection('/users/').doc(user.uid).update({
+    .collection('/users/').doc(user.uid).set({
+      firstname: '',
+      lastname: '',
       email: user.email,
-      imgurl:'https://ssl.gstatic.com/images/branding/product/1x/avatar_square_blue_512dp.png',
-      role:'user'
+      role: 'client',
+      imgurl: '',
+      team: '',
+      phone: '',
     });
+});
+
+exports.triggerUserDeleted = functions.auth.user().onDelete((user) => {
+  console.log("user deleted");
+  return admin
+    .firestore()
+    .collection('/users/').doc(user.uid).delete();
 });
