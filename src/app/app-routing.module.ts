45bae01ea32bd8b6d29779611c5eff4ee645550f
Dany Sanchez <daany_57@hotmail.fr>
2020-03-11 09:50:19 +0100

    :lipstick: Suppression bouton register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
     ]
   },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  // { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
